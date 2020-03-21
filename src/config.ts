1525e703e35315563d383980b822d3d2ad21df09
Matej Strnad <matej.strnad.97@gmail.com>
2020-03-21 15:01:14 +0100

    use production API url, use npm instead of yarn

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,2 @@
 export const API_KEY = process.env.APP_API_KEY
-export const API_URL =
-  'https://private-anon-ae30dbde22-strvtestprojectv2.apiary-proxy.com'
+export const API_URL = 'https://testproject-api-v2.strv.com'
